bf3ab7445b8db3f7a45a51e39a411cb637bd1d66
VictoriaAlekseeva <vicalekseeva88@gmail.com>
2024-02-12 21:26:00 +0100

    doc: add readme

--- src/dataBase.ts
+++ src/dataBase.ts
@@ -8,16 +8,16 @@ export interface User {
 export const db: User[] = [
   {
       "id": "b6e4cdb0-b296-4df4-8bd4-b0f4b12c1be0",
-      "username": "6543",
+      "username": "John",
       "age": 8,
       "hobbies": [
-          "",
+          "football",
           "golf"
       ]
   },
   {
       "id": "a82ca83f-03b8-4bad-9742-62f9d230dfb7",
-      "username": "6543test",
+      "username": "John sr",
       "age": 98,
       "hobbies": [
           "football",
